c65b82fc58b8e93c534a8bed15f61c9da45110ed
Sefa Kilic <sefa1@umbc.edu>
2016-02-27 00:51:39 -0500

    Rename directory

--- cgb/gene.py
+++ cgb/gene.py
@@ -68,7 +68,7 @@ class Gene:
         return self._product_feature.type if self._product_feature else ''
 
     def find_homolog_in_genome(self, genome):
-        """Returns the homolog gene in the given genome."""
+        """Returns the homologous gene in the given genome."""
         return genome.find_gene_homolog(self)
 
     def distance(self, other):
