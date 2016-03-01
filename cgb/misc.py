ab04563574c3c135c12e9c5a0b5dd10c33aebe50
Sefa Kilic <sefa1@umbc.edu>
2016-03-01 13:51:03 -0500

    Add weighted random choice

--- cgb/misc.py
+++ cgb/misc.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 
 
 def mean(xs):
@@ -9,3 +10,13 @@ def mean(xs):
 def log2(x):
     """Returns the logarithm in base 2."""
     return math.log(x, 2)
+
+
+def weighted_choice(xs, weights, count=1):
+    """Given a list of elements, randomly selects one based on weights.
+    Args:
+        xs (list): elements
+        weights (list): weights
+    """
+    ps = [float(w)/sum(weights) for w in weights]
+    return np.random.choice(xs, size=count, p=ps)
