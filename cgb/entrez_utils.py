fffbc0f643f698369f893950a9978a6c1fe21111
Sefa Kilic <sefa1@umbc.edu>
2016-03-01 12:53:17 -0500

    Locally store genbank files

--- cgb/entrez_utils.py
+++ cgb/entrez_utils.py
@@ -1,13 +1,26 @@
+import os
+
 from Bio import Entrez
+from Bio import SeqIO
 
 Entrez.email = 'sefa1@umbc.edu'
+
+ENTREZ_DIRECTORY = '/Users/sefa/Downloads/'
 
 
 def get_genome_record(accession):
     """Gets the genome record from NCBI RefSeq."""
-    handle = Entrez.efetch(db='nuccore', id=accession,
-                           retmode='gbwithparts', rettype='text')
-    return handle.read()
+    genbank_file = os.path.join(ENTREZ_DIRECTORY, accession+'.gb')
+    if not os.path.isfile(genbank_file):
+        # Download and save Genbank record
+        handle = Entrez.efetch(db='nuccore', id=accession,
+                               retmode='gbwithparts', rettype='text')
+        with open(genbank_file, 'w') as f:
+            f.write(handle.read())
+
+    record = SeqIO.read(open(genbank_file), 'gb')
+    return record
+
 
 
 def get_protein_record(accession):
