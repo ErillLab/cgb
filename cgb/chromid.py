8babeb99a3680bf08dfd9451df2c3d5f469ba3cd
ivanerill <erill@umbc.edu>
2019-04-09 00:25:45 -0400

    patching up trickle down of user_input for API wait

--- cgb/chromid.py
+++ cgb/chromid.py
@@ -33,6 +33,7 @@ class Chromid:
         raw_record = entrez_utils.get_genome_record(accession_number, user_input)
         self._record = SeqIO.read(cStringIO.StringIO(raw_record), 'gb')
         self._genome = genome
+        self._user_input = user_input
 
     @property
     def genome(self):
@@ -83,6 +84,10 @@ class Chromid:
         random_promoter = weighted_choice(promoters, map(len, promoters))
         start = random.randint(0, len(random_promoter)-length)
         return random_promoter[start:start+length]
+    
+    def user_input(self):
+        """Returns the user_input stored in chromid"""
+        return (self._user_input)
 
     @cached_property
     def length(self):
@@ -117,7 +122,7 @@ class Chromid:
                                       locus_tag)
                     index += 1
                     continue
-                gene_list.append(Gene(index, self, f, product_f))
+                gene_list.append(Gene(index, self, f, self.user_input, product_f))
                 index += 1
         return gene_list
 
